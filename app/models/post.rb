88829e3bffc237e74bc2285dc0ba3f64fbc141d6
Craig Hanna <craighanna@gmail.com>
2011-10-05 10:59:01 -0700

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+  has_many :comments
+  validates :title, :body, :presence => true
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'de919a6dabf1692694ce8c31ba632a8bb5163d603bb298df194288c57f3f6c6cd92b798898dfe67701415745342d813529870dcc9154866be759e7aea0e84adf'
